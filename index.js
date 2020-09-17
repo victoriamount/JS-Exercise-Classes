f80f0b7a8d27c37c2ee5a0857a2eb68ded9eaf88
Victoria Mount <victoriamount01@gmail.com>
2020-09-17 14:48:24 -0500

    Set up Classes project

--- index.js
+++ index.js
@@ -39,6 +39,8 @@ class Airplane {
     - Give instances of Person a method `.toString()`:
         + It should return a string with `name` and `age`. Example: "Mary, 50"
 */
+console.log("Task 1: ");
+
 
 class Person {
 
@@ -57,6 +59,8 @@ class Person {
     - A car which runs out of `fuel` while driving can't drive any more distance:
         + The `drive` method should return a string "I ran out of fuel at x miles!" x being `odometer`.
 */
+console.log("Task 2: ");
+
 
 class Car {
 
@@ -74,6 +78,9 @@ class Car {
         + Speaking should return a phrase `Hello my name is {name}, I am from {location}`.
         + {name} and {location} of course come from the instance's own properties.
 */
+console.log("Task 3: ");
+
+
 class Lambdasian {
 
 }
@@ -92,6 +99,9 @@ class Lambdasian {
         + `demo` receives a `subject` string as an argument and returns the phrase 'Today we are learning about {subject}' where subject is the param passed in.
         + `grade` receives a `student` object and a `subject` string as arguments and returns '{student.name} receives a perfect score on {subject}'
 */
+console.log("Task 4: ");
+
+
 class Instructor {
 
 }
@@ -111,6 +121,9 @@ class Instructor {
         + `PRAssignment` a method that receives a subject as an argument and returns `student.name has submitted a PR for {subject}`
         + `sprintChallenge` similar to PRAssignment but returns `student.name has begun sprint challenge on {subject}`
 */
+console.log("Task 5: ");
+
+
 class Student {
 
 }
@@ -128,6 +141,9 @@ class Student {
         + `standUp` a method that takes in a slack channel and returns `{name} announces to {channel}, @channel standy times!`
         + `debugsCode` a method that takes in a student object and a subject and returns `{name} debugs {student.name}'s code on {subject}`
 */
+console.log("Task 6: ");
+
+
 class ProjectManager {
 
 }
@@ -140,6 +156,7 @@ class ProjectManager {
       + This method, when called, will check the grade of the student and see if they're ready to graduate from Lambda School
       + If the student's grade is above a 70% let them graduate! Otherwise go back to grading their assignments to increase their score.
 */
+console.log("Stretch Problem: ");
 
 ///////// END OF CHALLENGE /////////
 ///////// END OF CHALLENGE /////////
